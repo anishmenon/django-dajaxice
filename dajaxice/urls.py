eb472fb98c02262483765268ffdfa4dc8523b5e5
Anish Menon <anish@inzane.in>
2014-01-08 20:22:08 +0530

    Fixed URL import error for latest versions of Django

--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -1,7 +1,16 @@
-from django.conf.urls.defaults import *
+import django
+if django.VERSION > 1.5:
+    from django.conf.urls import patterns, include, url
+else:
+    from django.conf.urls.defaults import *
+
+    
+    
+    
 from .views import DajaxiceRequest
 
 urlpatterns = patterns('dajaxice.views',
     url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
     url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
 )
+
